3c11ffce60acf7e6bbe047bfe3bb7fa634d837e0
Rodolpho Gomes <rodolpho.gda@gmail.com>
2024-11-22 02:35:24 -0300

    Created Database Postgres and First Method

--- src/main/java/com/rodolpho/parking_control/ParkingControlApiApplication.java
+++ src/main/java/com/rodolpho/parking_control/ParkingControlApiApplication.java
@@ -2,12 +2,20 @@ package com.rodolpho.parking_control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApiApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "Olá Mundo!";
 	}
 
 }
