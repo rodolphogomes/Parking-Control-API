a0e3e6e5ef35c227894cf74241cf46109bf774a8
Rodolpho Gomes <rodolpho.gda@gmail.com>
2024-11-22 15:45:46 -0300

    Created ParkingSpotController class in package Controller

---
+++ src/main/java/com/rodolpho/parking_control/controllers/ParkingSpotController.java
@@ -0,0 +1,22 @@
+package com.rodolpho.parking_control.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodolpho.parking_control.services.ParkingSpotService;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+	
+	final ParkingSpotService parkingSpotService;
+	
+	public ParkingSpotController(ParkingSpotService parkingSpotService) {
+		this.parkingSpotService = parkingSpotService;
+	}
+	
+	
+
+}
